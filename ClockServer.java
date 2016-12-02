4be7614417c848db2805f9afe98a31193e3692f6
maxiwen <maxiwen521@126.com>
2016-12-02 10:59:49 +0900

    javaのpgm

---
+++ ClockServer.java
@@ -0,0 +1,52 @@
+import	java.io.*;
+import	java.net.*;
+import	java.util.*;
+
+public	class	ClockServer{
+	public static void	main(String[]	args){
+		// 宣言。
+		ServerSocket	servsock	=	null;	// サーバ用ソケット。
+		Socket	sock;					// 読み書き用ソケット。
+		OutputStream	out;				// 出力ストリーム。
+		String	outstr;
+		int	i;
+		Date	d;					// 日付をハンドルするオブジェクト。
+
+		// サーバ動作。
+		try	{
+			// サーバソケットを作成する。
+			servsock	=	new	ServerSocket(6000,	300);
+
+			// 接続の待ち受けと処理の繰り返し。
+			while(true)
+			{
+				sock	=	servsock.accept();		// 接続受付。
+
+				// 出力用データを生成する。
+				d	=	new	Date();
+				outstr	=	"\n"
+						+	"Hello, this is ClockServer by baseibun."
+						+	"\n"	
+						+	"kyou ha ri ketu san no tanjyoubi de su."	
+						+	"\n"
+						+	"o tanjyoubi o me de tou."
+						+	"\n";
+
+				// データを出力する。
+			out	=	sock.getOutputStream();
+
+			for(i=0;i<outstr.length();	i++)
+					{
+						out.write((int)	outstr.charAt(i));
+					}
+					out.write('\n');
+
+					// ストリームを閉じる。
+					sock.close();
+				}
+			}
+			catch	(IOException	e)	{
+				System.exit(1);
+			}
+		}
+	}
