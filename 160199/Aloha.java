44e17d8c6141cc168bff6fb4a3ab5c5ef5b8dfbd
Eieisan <160144@yses.ac.jp>
2016-12-06 09:29:06 +0900

    Aloha

---
+++ 160199/Aloha.java
@@ -0,0 +1,11 @@
+public class Aloha{
+	public static void main(String[] args) {
+		for(int i=0; i<20;i++){
+		if(i%4==0){
+		System.out.println("ｱﾛﾊ");
+	}else{
+		System.out.println("Aloha!");
+	}
+	}
+	}
+}
