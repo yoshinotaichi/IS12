8803f0dbbad0340e0c2983f3e602f8788cd15be3
santoshsantosh <yse160149@gmail.com>
2016-12-06 09:48:55 +0900

    ChatClient.java を　作りました。

---
+++ 160149_java/ChatClient.java
@@ -0,0 +1,13 @@
+import java.net.*;
+ import java.io.*;
+public class ChatClient{
+	public static void main(String[] args) {
+		System.out.println("Namaste!");
+	}
+}
+class T1{
+
+}
+class StreamConnector{
+
+ }
