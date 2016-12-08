76c18a9c1d28cf719b6dda9b53b9f12c677b2d6b
Eieisan <160144@yses.ac.jp>
2016-12-08 11:31:36 +0900

    ４回目

---
+++ 20161208/CopyByteImage.java
@@ -0,0 +1,24 @@
+import java.io.*;
+public class CopyByteImage{
+	public static void main(String[] args) {
+		FileInputStream inFile=null;			
+
+		try{
+		inFile = new FileInputStream("fruit.jpg");
+		//int data=inFile.read();
+		int n=0;
+		int data;
+
+		while((data=inFile.read())!=-1){
+		System.out.println("n:"+n+"3");
+		n++;
+	}
+		inFile.close();
+
+	}catch (IOException e){
+		System.err.println("エラーです。");
+		System.exit(1);
+		}
+		
+	}
+}
