e77459e7314aa6b73036d13be383e57d4463bcc6
iamnoone999 <160139@yses.ac.jp>
2016-12-12 14:24:45 +0900

    streamをつくりました

--- 20161212_160139/Aloha.java
+++ 20161212_160139/Aloha.java
@@ -1,5 +1,21 @@
+import java.io.*;
 public class Aloha{
-	public static void main(String[] args) {
-		System.out.println("Aloha!");
+	public static void main(String[]args){
+FileInputStream inFile = null;
+
+try{
+	inFile = new FileInputStream(fileName);
+	//int data = inFile.read();
+	int n=0;
+	int data;
+	while((data = inFile.read())!=-1){
+	System.out.println(n+":"+data);
+	n++;
 	}
+	inFile.close();
+}catch(IOException e){
+	System.err.println("エラーです");
+	System.exit(1);
+		}
+}
 }
