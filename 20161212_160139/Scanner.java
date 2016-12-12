740f83d9487813d9d3fd3416c9a3e8a9dd7b13f1
iamnoone999 <160139@yses.ac.jp>
2016-12-12 13:23:22 +0900

    Aloha, Args,Scannerをつくりました

---
+++ 20161212_160139/Scanner.java
@@ -0,0 +1,20 @@
+public class Scanner {
+	public static void main (String[] args) {
+	
+	String name;
+
+	while (true){
+	System.out.println("名前を入力してください >");
+
+
+	name = new java.util.Scanner(System.in).nextLine();
+	System.out.println(name);
+	
+if (name.equals("") ){
+	   break;
+
+}
+
+	}
+}
+	}
